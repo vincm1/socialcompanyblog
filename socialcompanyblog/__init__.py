2eb08b6453e6318104d6994da258d1ccb1d49d31
vincm1 <vincent.michler94@gmail.com>
2022-08-23 19:41:11 +0200

    model setup

--- socialcompanyblog/__init__.py
+++ socialcompanyblog/__init__.py
@@ -1,7 +1,25 @@
 # Social_companyBlog/__init__.py
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+print(basedir)
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
 
 from socialcompanyblog.core.views import core
 from socialcompanyblog.error_pages.handlers import error_pages
